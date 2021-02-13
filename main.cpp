1af8af626b43b9ddc5a89ded50095d25fd9ac239
Aaditya Singh <aaditya.singh.met16@itbhu.ac.in>
2021-02-13 11:56:25 +0530

    working qt Image Viewer with zoom buttons.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
-#include <iostream>
-#include "opencv4/opencv2/opencv_modules.hpp"
-#include "opencv4/opencv2/opencv.hpp"
-#include "opencv4/opencv2/core.hpp"
+#include <QApplication>
+#include "mainwindow.h"
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    try
-    {
-        cv::Mat src_host = cv::imread("file.png", CV_LOAD_IMAGE_GRAYSCALE);
-
-        cv::Mat result_host;
-
-
-    }
-    catch(const cv::Exception& ex)
-    {
-        std::cout << "Error: " << ex.what() << std::endl;
-    }
-    return 0;
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.setWindowTitle("ImageViewer");
+    window.show();
+    return app.exec();
 }
---
+++ mainwindow.h
@@ -0,0 +1,67 @@
+//
+// Created by aaditya on 13/02/21.
+//
+
+#ifndef WASH_MAINWINDOW_H
+#define WASH_MAINWINDOW_H
+
+
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QAction>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QStatusBar>
+#include <QLabel>
+#include <QGraphicsPixmapItem>
+
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent=nullptr);
+    ~MainWindow();
+
+private:
+    void initUI();
+    void createActions();
+    void showImage(QString);
+    void setupShortcuts();
+
+private slots:
+    void openImage();
+    void zoomIn();
+    void zoomOut();
+    void prevImage();
+    void nextImage();
+    void saveAs();
+
+private:
+    QMenu *fileMenu;
+    QMenu *viewMenu;
+
+    QToolBar *fileToolBar;
+    QToolBar *viewToolBar;
+
+    QGraphicsScene *imageScene;
+    QGraphicsView *imageView;
+
+    QStatusBar *mainStatusBar;
+    QLabel *mainStatusLabel;
+
+    QAction *openAction;
+    QAction *saveAsAction;
+    QAction *exitAction;
+    QAction *zoomInAction;
+    QAction *zoomOutAction;
+    QAction *prevAction;
+    QAction *nextAction;
+
+    QString currentImagePath;
+    QGraphicsPixmapItem *currentImage;
+};
+
+
+#endif //WASH_MAINWINDOW_H
